8fb2ecdb59064c3959c436f8e6be11a8487eeeb9
rpzbass <ronaldop.tech@gmail.com>
2021-08-06 17:44:07 -0300

    Adicionando notações do Spring

--- backend/src/main/java/com/agendamento/barbaman/entities/Especialidade.java
+++ backend/src/main/java/com/agendamento/barbaman/entities/Especialidade.java
@@ -2,19 +2,30 @@ package com.agendamento.barbaman.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
 @Entity
 @Table(name = "tb_especialidade")
 public class Especialidade implements Serializable {
 	
-
+	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column(name = "id")
 	private Long id;
+	
 	private String nome;
 	private Double valor;
 	private String descricao;
+	
 	
 	
 	public Especialidade() {
--- backend/src/main/java/com/agendamento/barbaman/entities/Profissional.java
+++ backend/src/main/java/com/agendamento/barbaman/entities/Profissional.java
@@ -4,14 +4,33 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_profissional")
 public class Profissional implements Serializable  {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String celular;
 	
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_profissional_especialidade",
+	joinColumns = @JoinColumn(name = "profissional_id"),
+	inverseJoinColumns = @JoinColumn(name = "especialidade_id"))
 	private Set<Especialidade> especialidades = new HashSet<>();
 	
 	
