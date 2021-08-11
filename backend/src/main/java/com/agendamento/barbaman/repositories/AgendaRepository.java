57eba3e8e0be268d694904c20952db93ac211efd
rpzbass <ronaldop.tech@gmail.com>
2021-08-10 21:10:17 -0300

    Add repository

--- backend/src/main/java/com/agendamento/barbaman/repositories/AgendaRepository.java
+++ backend/src/main/java/com/agendamento/barbaman/repositories/AgendaRepository.java
@@ -1,7 +1,6 @@
 package com.agendamento.barbaman.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.agendamento.barbaman.entities.Agenda;
 
 public interface AgendaRepository extends JpaRepository<Agenda,Long> {
